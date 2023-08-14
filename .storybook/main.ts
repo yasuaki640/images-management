030d6558a0da07161f60083a7e261b2ff07f5a1c
yasuaki640 <yasuaki640@gmail.com>
2023-08-14 09:09:11 +0900

    fuck vue

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -22,7 +22,7 @@ const config: StorybookConfig = {
     mergeConfig(config, {
       server: {
         watch: {
-          ignored: ["**/.env"]
+          ignored: ["**/.env", "**/.env.*"]
         }
       }
     })
