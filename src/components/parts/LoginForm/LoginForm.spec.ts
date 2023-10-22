316734716ea14809c76adf5ea159a41b8ca5cf58
yasuaki640 <yasuaki640@gmail.com>
2023-10-22 12:44:00 +0900

    コンポーネントのテストやっぱり癖あるなあ、、

---
+++ src/components/parts/LoginForm/LoginForm.spec.ts
@@ -0,0 +1,32 @@
+import { describe, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import LoginForm from "@/components/parts/LoginForm/LoginForm.vue";
+
+describe("LoginForm", () => {
+  it("can not click send button in initial view.", async () => {
+    const wrapper = mount(LoginForm, {
+      props: {
+        email: "",
+        sending: false
+      }
+    });
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.emitted()).not.toHaveProperty("sendMagicLink");
+  });
+
+  it("displays error if inputted wrong format email.", async () => {
+    const wrapper = mount(LoginForm, {
+      props: {
+        email: "",
+        sending: false
+      }
+    });
+
+    await wrapper.find("input").setValue("wrong email");
+    await wrapper.find("input").trigger("blur");
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    expect(wrapper.text()).toContain("Please input correct mail address");
+  });
+});
