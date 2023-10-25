c69a2776a0ed74b407593eebf1e607761785296e
yasuaki640 <yasuaki640@gmail.com>
2023-10-25 09:02:01 +0900

    コンポーネントテスト書きづれえええええええええ

--- src/components/parts/LoginForm/LoginForm.spec.ts
+++ src/components/parts/LoginForm/LoginForm.spec.ts
@@ -10,7 +10,7 @@ describe("LoginForm", () => {
         sending: false
       }
     });
-    await wrapper.find("button").trigger("click");
+    await wrapper.get("button").trigger("click");
     expect(wrapper.emitted()).not.toHaveProperty("sendMagicLink");
   });
 
@@ -22,11 +22,46 @@ describe("LoginForm", () => {
       }
     });
 
-    await wrapper.find("input").setValue("wrong email");
-    await wrapper.find("input").trigger("blur");
+    await wrapper.get("input").setValue("wrong email");
+    await wrapper.get("input").trigger("blur");
 
     await new Promise((resolve) => setTimeout(resolve, 100));
-
     expect(wrapper.text()).toContain("Please input correct mail address");
+
+    await wrapper.get("button").trigger("click");
+    expect(wrapper.emitted()).not.toHaveProperty("sendMagicLink");
+  });
+
+  it("can not click submit button if inputted wrong format email.", async () => {
+    const wrapper = mount(LoginForm, {
+      props: {
+        email: "",
+        sending: false
+      }
+    });
+
+    await wrapper.get("input").setValue("wrong email");
+    await wrapper.get("input").trigger("blur");
+    await wrapper.get("button").trigger("click");
+
+    expect(wrapper.emitted()).not.toHaveProperty("sendMagicLink");
+  });
+
+  it("can submit with email.", async () => {
+    const wrapper = mount(LoginForm, {
+      props: {
+        email: "",
+        sending: false
+      }
+    });
+
+    await wrapper.get("input").setValue("test@example.com");
+    await wrapper.setProps({ email: "a@a.com" });
+    await wrapper.get("input").trigger("blur");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await wrapper.get("button").trigger("click");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    expect(wrapper.emitted()).toHaveProperty("sendMagicLink");
   });
 });
